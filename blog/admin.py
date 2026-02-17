52d916d0da4cc843bd3b2e00ab4afa9132fb4542
alanbispo <alannmg1@gmail.com>
2026-02-17 15:05:08 -0300

    feat: add categoria aos posts

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Categoria
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('nome',)}
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'autor', 'criado_em')
+    list_display = ('titulo', 'categoria', 'autor', 'criado_em')
     prepopulated_fields = {'slug': ('titulo',)}
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.nome
 class Post(models.Model):
     titulo = models.CharField(max_length=200)
     slug = models.SlugField(unique=True) 
@@ -8,6 +14,7 @@ class Post(models.Model):
     conteudo = models.TextField()
     criado_em = models.DateTimeField(auto_now_add=True)
     atualizado_em = models.DateTimeField(auto_now=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.titulo
