b5ac41d8f01b4c53180168f4726acafd850ca84c
alanbispo <alannmg1@gmail.com>
2026-02-17 15:18:12 -0300

    feat: add filtro de categorias e nome

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Categoria
 
 def home(request):
-    posts = Post.objects.all()
-    
-    return render(request, 'blog/home.html', {'posts': posts})
+    busca = request.GET.get('busca', '')
+    categoria_slug = request.GET.get('categoria', '')
+
+    posts = Post.objects.all().order_by('-criado_em')
+
+    if busca:
+        posts = posts.filter(titulo__icontains=busca)
+
+    if categoria_slug:
+        posts = posts.filter(categoria__slug=categoria_slug)
+
+    categorias = Categoria.objects.all()
+
+    context = {
+        'posts': posts,
+        'categorias': categorias,
+        'busca': busca,
+    }
+
+    return render(request, 'blog/home.html', context)
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
